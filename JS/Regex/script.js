9d6cfb99d9be6733f2c5eb81cfb278f702f5d8ba
AdamPRo <azerosson@gmail.com>
2026-03-05 17:04:46 +0100

    Amélioration du formulaire: regex, HTML lisible et CSS modernisé

--- JS/Regex/script.js
+++ JS/Regex/script.js
@@ -1,24 +1,64 @@
-nom = getElementById("name")
-prenom = getElementById("prenom")
-mail = getElementById("mail")
-date = getElementById("date")
-date2 = getElementById("date2")
-cp = getElementById("cp")
-tel = getElementById("tel")
-mpd = getElementById("mpd")
+lesinput = document.querySelectorAll("input");
 
-nom.addeventlistener("change",verifNom) //addEventListener
+lesinput.forEach(element => {
+    element.addEventListener("change", regex);
+});
 
-function verifNom(){
-    if(nom.value.length < 2){
-        alert("Le nom doit contenir au moins 2 caractères")
+function regex(e) {
+     cible = e.target;
+    if(!cible.checkValidity()==true) {
+        cible.classList.remove("valid");
+        cible.classList.add("erreur");
+    } else {
+        cible.classList.remove("erreur");
+        cible.classList.add("valid");
     }
 }
 
-prenom.addeventlistener("change",verifPrenom)  //addEventListener
 
-function verifPrenom(){
-    if(prenom.value.length < 2){
-        alert("Le prénom doit contenir au moins 2 caractères")
-    }
+mdp = document.getElementById("mdp");
+
+mdp.addEventListener("input",regexMdp);
+function regexMdp(e) {
+     valeur = e.target.value;
+maj = document.querySelector("#maj")
+min = document.querySelector("#min")
+num = document.querySelector("#num")
+long = document.querySelector("#long")
+
+
+    
+    if (/[A-Z]/.test(valeur)) {
+        maj.textContent = "✔️ Une majuscule";
+        maj.style.color = "green";
+} else{
+       maj.textContent = "❌ Une majuscule";
+        maj.style.color = "red";
+    
 }
+
+      if (/[a-z]/.test(valeur)) {
+        min.textContent = "✔️ Une minuscule";
+        min.style.color = "green";
+} else{
+       min.textContent = "❌ Une minuscule";
+        min.style.color = "red";
+    
+}
+    
+    if (/[0-9]/.test(valeur)) {
+        num.textContent = "✔️ Un chiffre";
+        num.style.color = "green";
+} else{
+       num.textContent = "❌ Un chiffre";
+        num.style.color = "red";
+    
+}    
+    if (/{8,}/.test(valeur)) {
+        long.textContent = "✔️ Au moins 8 caractères";
+        long.style.color = "green";
+} else{
+       long.textContent = "❌ Au moins 8 caractères";
+        long.style.color = "red";
+    
+}}
