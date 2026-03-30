bc3688e0d740cb7af20d023bb5590218ee9e1d4c
AdamPRo <azerosson@gmail.com>
2026-03-30 12:04:00 +0200

    30/03/2026 12:04:00,46

---
+++ ECFEVAL_Rattr_JS_Yousri/script.js
@@ -0,0 +1,16 @@
+option=document.getElementById('option' );
+function chgColeur (option){
+        option.addEventListener("click",function(e){
+        if ("click"){  click = true
+                option.classList.add("selected");
+       
+
+         }
+         else { click=false
+                option.classList.remove("selected");
+          }
+
+});
+
+
+}
--- Javascript/Patapon/paflechien.js
+++
@@ -1,6 +0,0 @@
-nbclick = 0
-const txt1= getElementById("txt1");
-
-monTexte.addEventListener('click',function(){
-    if(txt1.paflechien)
-}
